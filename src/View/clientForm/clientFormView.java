cc1f86e98a64b1df88f91662e66a3f4e0ddc8627
Ian Smith <ismith14@students.towson.edu>
2022-04-21 19:18:48 -0400

    Forgot to remove main method again

--- src/View/clientForm/clientFormView.java
+++ src/View/clientForm/clientFormView.java
@@ -328,17 +328,5 @@ public class clientFormView extends JPanel implements ActionListener {
     public JButton getClientUpdate() {
         return clientUpdate;
     }
-    public static void main(String[] args) {
-        //type in create or read for different forms
-        String a = "Read";
-        clientFormView f = new clientFormView(a);
-        JFrame j = new JFrame();
-
-        j.setTitle("Form");
-        j.setSize(600,500);
-        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        j.setVisible(true);
-        j.add(f);
-    }
 }
 
