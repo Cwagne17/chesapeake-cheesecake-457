e2b3169716556b6a9505e91740f4957477885684
Cwagne17 <cwagne17@students.towson.edu>
2022-05-10 10:49:58 -0400

    finished

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
-
-import Config.errors.StringException;
-import Model.ClientModel;
-import Model.entities.Client;
 import View.MainFrame;
 
 import javax.swing.*;
-import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) {
